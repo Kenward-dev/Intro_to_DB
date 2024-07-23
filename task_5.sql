a7ffddad23eff3039b6c7abfb9b47aa437816596
Kenward Terhemba <codewithkenward@gmail.com>
2024-07-23 20:27:10 +0100

    a script that inserts a single row in the table 'customer' (database alx_book_store) in your MySQL server.

---
+++ task_5.sql
@@ -0,0 +1,19 @@
+import mysql.connector
+
+db = mysql.connector.connect(
+	host="localhost",
+    user="root",
+    password="dogtorken",
+    database="alx_book_store"
+)
+
+mycursor = db.cursor()
+
+sql = "INSERT INTO Customer(customer_id, custormer_name, email, address) VALUES (%s, %s, %s, %s)"
+val = (1, "Cole Baidoo", "cbaidoo@sandtech.com", "123 Happiness Ave.")
+
+mycursor.execute(sql, val)
+db.commit()
+
+mycursor.close()
+mydb.close()
